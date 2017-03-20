548cb49c9502e192fe672b03abb7fdc4bb8e4c51
adeept <adeept@163.com>
2017-03-20 07:38:18 +0000

    add ADC fun

--- 17_flame/flame.c
+++ 17_flame/flame.c
@@ -9,7 +9,60 @@
 #include <wiringPi.h>
 #include <stdio.h>
 
-#define Flame_S  0
+typedef unsigned int  uint;
+typedef unsigned char uchar;
+
+#define     ADC_CS    0
+#define     ADC_DIO   1
+#define     ADC_CLK   2
+#define     Flame_S   3
+
+uchar get_ADC_Result(void)
+{
+	//10:CH0
+	//11:CH1
+	uchar i;
+	uchar dat1=0, dat2=0;
+
+	digitalWrite(ADC_CS, 0);
+
+	digitalWrite(ADC_CLK,0);
+	digitalWrite(ADC_DIO,1);	delayMicroseconds(2);
+	digitalWrite(ADC_CLK,1);	delayMicroseconds(2);
+	digitalWrite(ADC_CLK,0);
+
+	digitalWrite(ADC_DIO,1);    delayMicroseconds(2);
+	digitalWrite(ADC_CLK,1);	delayMicroseconds(2);
+	digitalWrite(ADC_CLK,0);
+
+	digitalWrite(ADC_DIO,0);	delayMicroseconds(2); //CH0
+	digitalWrite(ADC_CLK,1);
+	digitalWrite(ADC_DIO,1);    delayMicroseconds(2);
+	digitalWrite(ADC_CLK,0);
+	digitalWrite(ADC_DIO,1);    delayMicroseconds(2);
+
+	for(i=0;i<8;i++)
+	{
+		digitalWrite(ADC_CLK,1);	delayMicroseconds(2);
+		digitalWrite(ADC_CLK,0);    delayMicroseconds(2);
+
+		pinMode(ADC_DIO, INPUT);
+		dat1=dat1<<1 | digitalRead(ADC_DIO);
+	}
+
+	for(i=0;i<8;i++)
+	{
+		dat2 = dat2 | ((uchar)(digitalRead(ADC_DIO))<<i);
+		digitalWrite(ADC_CLK,1); 	delayMicroseconds(2);
+		digitalWrite(ADC_CLK,0);    delayMicroseconds(2);
+	}
+
+	digitalWrite(ADC_CS,1);
+
+	pinMode(ADC_DIO, OUTPUT);
+
+	return(dat1==dat2) ? dat1 : 0;
+}
 
 void myISR(void)
 {
@@ -18,12 +71,16 @@ void myISR(void)
 
 int main(void)
 {
+	int res;
+	
 	if(wiringPiSetup() == -1){
 		printf("setup wiringPi failed !\n");
 		return -1; 
 	}
 	
 	pinMode(Flame_S, INPUT);
+	pinMode(ADC_CS,  OUTPUT);
+	pinMode(ADC_CLK, OUTPUT);
 
 	pullUpDnControl(Flame_S, PUD_UP);
 	
@@ -33,7 +90,8 @@ int main(void)
 	}
 
 	while(1){
-		printf("test...\n");
+		res = get_ADC_Result();
+		printf("analog = %d\n", res);
 		delay(100);
 	}
 
